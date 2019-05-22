8a08018c669c68131a8e2b20eff468a8c2d11724
Paul Lovato <plovato@gmail.com>
2019-05-21 19:28:54 -0600

    Home of Recruitment Board.

--- components/Home.php
+++ components/Home.php
@@ -2,7 +2,11 @@
 
 namespace Cleanse\Recruitment\Components;
 
+use Auth;
 use Cms\Classes\ComponentBase;
+
+use Cleanse\Recruitment\Models\Player;
+use Cleanse\Recruitment\Models\Team;
 
 class Home extends ComponentBase
 {
@@ -16,6 +20,21 @@ class Home extends ComponentBase
 
     public function onRun()
     {
-        //
+        if (isset(Auth::getUser()->id)) {
+            $this->page['user_teams'] = $this->getUserTeams();
+            $this->page['user_characters'] = $this->getUserPlayers();
+        }
+
+        $this->addCss('assets/css/recruitment.css');
+    }
+
+    private function getUserTeams()
+    {
+        return Team::where('user_id', '=', Auth::getUser()->id)->get();
+    }
+
+    private function getUserPlayers()
+    {
+        return Player::where('user_id', '=', Auth::getUser()->id)->get();
     }
 }
